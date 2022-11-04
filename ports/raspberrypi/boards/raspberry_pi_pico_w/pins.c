e02cb71880507b39e5aaf3568c4e50873bbb98e4
Georg Bøe <georg@georgboe.com>
2022-11-04 22:33:22 +0100

    Add pin 24

--- ports/raspberrypi/boards/raspberry_pi_pico_w/pins.c
+++ ports/raspberrypi/boards/raspberry_pi_pico_w/pins.c
@@ -26,6 +26,7 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_GP20), MP_ROM_PTR(&pin_GPIO20) },
     { MP_ROM_QSTR(MP_QSTR_GP21), MP_ROM_PTR(&pin_GPIO21) },
     { MP_ROM_QSTR(MP_QSTR_GP22), MP_ROM_PTR(&pin_GPIO22) },
+    { MP_ROM_QSTR(MP_QSTR_GP24), MP_ROM_PTR(&pin_GPIO24) },
 
     { MP_ROM_QSTR(MP_QSTR_SMPS_MODE), MP_ROM_PTR(&pin_CYW1) },
 
